2d9b222af76592412e30b376fbfb1a7af087211a
unknown <195132@edu.p.lodz.pl>
2018-01-15 13:44:41 +0100

    Przeniesiony pliki z branchy email na mastera

---
+++ IOMail/Interfaces/IEmail.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IOMail.Data;
+
+namespace IOMail.Interfaces
+{
+    public interface IEmail
+    {
+        EmailData Data { get; set; }
+        ITemplateParser Parser { get; set; }
+        ISender Sender { get; set; }
+
+        IEmail To(string emailAddress);
+        //IEmail From(string emailAddress);
+        IEmail Subject(string subject);
+        IEmail Body(string body);
+        IEmail UseTemplate(string templatename, dynamic data);
+        IEmail Send();
+    }
+}
---
+++ IOMail/SenderSMTP.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Mail;
+using IOMail.Data;
+using IOMail.Interfaces;
+
+namespace IOMail
+{
+    public class SenderSmtp : ISender
+    {
+        private SmtpClient smtp;
+        const string fromPassword = "ioproject";
+
+        public SenderSmtp()// : this(new SmtpClient())
+        {
+            smtp = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false
+            };
+        }
+
+        public SenderSmtp(SmtpClient client)
+        {
+            smtp = client;
+        }
+
+        public void Send(IEmail email)
+        {
+            var message = CreateMessage(email);
+            smtp.Credentials = new NetworkCredential(message.From.Address, fromPassword);
+
+            smtp.Send(message);
+            smtp?.Dispose();
+        }
+
+        private MailMessage CreateMessage(IEmail email)
+        {
+            var data = email.Data;
+            var message = new MailMessage
+            {
+                Subject = data.Subject,
+                Body = data.Body,
+                From = new MailAddress(data.FromAddress.EmailAddress, "ioproject")
+            };
+
+            message.To.Add(new MailAddress(data.ToAddress.EmailAddress, "customer"));
+
+            return message;
+        }
+    }
+}
