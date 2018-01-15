0725eb003523e7b2d642eaa385ef3094d3b64b57
Andrzej <andrzej.piatkowski96@gmail.com>
2018-01-16 00:15:06 +0100

    products cd

--- BankProducts/View/MainProductWindow.xaml.cs
+++ BankProducts/View/MainProductWindow.xaml.cs
@@ -24,26 +24,30 @@ namespace BankProducts.View
     /// </summary>
     public partial class MainProductWindow : Window
     {
-        private ClientRepository repository = null;
         private Card card = null;
-        private Account account = null;
-        private Client client = null;
+        private Client clientDetails = null;
         private List<String> listaWalut = new List<String>();
         private List<String> typyKont = new List<String>();
+        public IEnumerable<Account> lista;
 
-        public MainProductWindow(ClientRepository repository, Client client)
+
+        public Client ClientDetailsProperty {
+            get { return clientDetails; }
+            set { clientDetails = value; }
+        }
+
+        public MainProductWindow()
         {
             InitializeComponent();
-            this.repository = repository;
-            this.client = client;
         }
-        
-        public void LoadDetailsClient(Client clientDetails)
-        { 
-            FirstName.Content = clientDetails.FirstName;
-            LastName.Content = clientDetails.LastName;
-            Email.Content = clientDetails.Email;
-            Pesel.Content = clientDetails.Pesel;
+
+        public void LoadClientToEdit(Client client)
+        {
+            this.clientDetails = client;
+            FirstName_Copy.Content = clientDetails.FirstName;
+            LastName_Copy.Content = clientDetails.LastName;
+            Email_Copy.Content = clientDetails.Email;
+            Pesel_Copy.Content = clientDetails.Pesel;
         }
 
         private void CancelProduct_Click(object sender, RoutedEventArgs e)
@@ -51,70 +55,38 @@ namespace BankProducts.View
             MessageBoxResult result = MessageBox.Show("Czy na pewno chcesz anulować?", "Potwierdzenie", MessageBoxButton.YesNo, MessageBoxImage.Question);
             if (result == MessageBoxResult.Yes)
             {
-                DialogResult = false;
                 Close();
             }
         }
 
         private void AddAccount_Click(object sender, RoutedEventArgs e)
         {
-            AddAccountToClientWindow addAccountToClientWindow = new AddAccountToClientWindow();       
-            if(addAccountToClientWindow.ShowDialog() == true)
-            {
-                listaWalut.Add("Polski złoty");
-                listaWalut.Add("Euro");
-                listaWalut.Add("Dolar amerykański");
-                addAccountToClientWindow.WalutaText.ItemsSource = listaWalut;
+            AddAccountToClientWindow addAccountToClientWindow = new AddAccountToClientWindow();
+            addAccountToClientWindow.Show();
+            listaWalut.Add("Polski złoty");
+            listaWalut.Add("Euro");
+            listaWalut.Add("Dolar amerykański");
+            addAccountToClientWindow.WalutaText.ItemsSource = listaWalut;
 
-                typyKont.Add("Regularne");
-                typyKont.Add("Złote");
-                typyKont.Add("Platynowe");
-                addAccountToClientWindow.TypText.ItemsSource = typyKont;
-
-
-                float rate = float.Parse(addAccountToClientWindow.OprocentowanieText.Text);
-                Currency currency;
-                AccountType accountType;
-                if ((string)addAccountToClientWindow.WalutaText.SelectedItem == "Polski złoty")
-                {
-                    currency = Currency.PLN;
-                }
-                else if ((string)addAccountToClientWindow.WalutaText.SelectedItem == "Euro")
-                {
-                    currency = Currency.EUR;
-                }
-                else
-                    currency = Currency.USD;
-
-                if ((string)addAccountToClientWindow.TypText.SelectedItem == "Regularne")
-                {
-                    accountType = AccountType.Regular;
-                }
-                else if ((string)addAccountToClientWindow.TypText.SelectedItem == "Złote")
-                {
-                    accountType = AccountType.Gold;
-                }
-                else
-                    accountType = AccountType.Platinum;
-
-                account = new Account(addAccountToClientWindow.NazwaKontaText.Text, currency, accountType, rate);
-            }
+            typyKont.Add("Regularne");
+            typyKont.Add("Złote");
+            typyKont.Add("Platynowe");
+            addAccountToClientWindow.TypText.ItemsSource = typyKont;
         }
 
         private void AddCard_Click(object sender, RoutedEventArgs e)
         {
             AddCardToClientWindow addCardToClientWindow = new AddCardToClientWindow();
-            if(addCardToClientWindow.ShowDialog() == true)
+            if (addCardToClientWindow.ShowDialog() == true)
             {
-                var lista = repository.getClientAccounts(client.Id);
                 List<Account> listaKontKlienta = (List<Account>)lista;
                 List<String> listaNazwKontKlienta = new List<string>();
-                foreach(Account konto in listaKontKlienta)
+                foreach (Account konto in listaKontKlienta)
                 {
                     listaNazwKontKlienta.Add(konto.Name);
                 }
                 addCardToClientWindow.NumerKontaText.ItemsSource = listaNazwKontKlienta;
-                card = new Card(client.Id, addCardToClientWindow.NumerKontaText.SelectedItem.ToString(), addCardToClientWindow.PINText.ToString());
+                card = new Card(clientDetails.Id, addCardToClientWindow.NumerKontaText.SelectedItem.ToString(), addCardToClientWindow.PINText.ToString());
             }
         }
 
